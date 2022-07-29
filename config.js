5db8610bdfc995667475d36f33c7b354e6f4d00b
TeguhGti <109796275+TeguhGti@users.noreply.github.com>
2022-07-29 15:15:31 +0700

    Update config.js
    W

--- config.js
+++ config.js
@@ -9,13 +9,14 @@
 // thank you to fernazer
 // thank you to MikeBot Dev Team
 // thank you to ALL Bot creator
+// thank you to Teguh
 // and thanks you to who support my Bot
 let fs = require('fs')
 let chalk = require('chalk')
 
 global.linkGC = ['https://chat.whatsapp.com/EVCGfzxLWfp81n0WhmVklH', 'https://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['6289505165400', '6282221792667', '6285730903853'] // Masukan nomot kalian
-global.mods = ['6289505165400', '6282221792667'] // Moderator
+global.owner = ['6282286192918', '6289505165400', '6282221792667', '6285730903853'] // Masukan nomot kalian
+global.mods = ['6282286192917', '6289505165400', '6282221792667'] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -54,8 +55,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'Alyaa#'
-  var sticker_author = '©AlyaaXzy'
+  var sticker_name = 'Teguh#'
+  var sticker_author = '©Teguh'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
